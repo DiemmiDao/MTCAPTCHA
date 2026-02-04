18b74d5c82f3060d2f81e2b1d4477f1db72db8e6
DiemmiDao <adao8@gmu.edu>
2026-02-04 11:18:45 -0500

    Delete config - Copy.js

--- config - Copy.js
+++
@@ -1,6 +0,0 @@
-// after you registered an account on MT CAPTCHA, put your site key and private key here
-
-module.exports = {
-  MTCAPTCHA_PRIVATE_KEY: process.env.MTCAPTCHA_PRIVATE_KEY,
-  MTCAPTCHA_SITE_KEY: process.env.MTCAPTCHA_SITE_KEY
-};
